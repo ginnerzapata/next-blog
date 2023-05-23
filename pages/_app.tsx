275deff1316c890eda6a90815357e9827dffe4d2
Ginner Zapata <gzapata@vizzn.ca>
2023-05-23 00:08:01 -0300

    unused imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,6 @@ import { Toaster } from "react-hot-toast";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 import { UserContext } from "../lib/contex";
-import { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth, firestore } from "../lib/firebase";
 import { useUserData } from "../lib/hooks";
 
 function MyApp({ Component, pageProps }) {
